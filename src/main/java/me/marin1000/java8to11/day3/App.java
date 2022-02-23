bd433c71d59a8dceca64f00da6087fac2508afbf
1000marin <leexjin@gmail.com>
2022-02-23 16:49:55 +0900

    Java 8to11 : Stream API

---
+++ src/main/java/me/marin1000/java8to11/day3/App.java
@@ -0,0 +1,26 @@
+package me.marin1000.java8to11.day3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class App {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("chuljin");
+        names.add("son");
+        names.add("toby");
+        names.add("foo");
+
+        List<String> collect = names.stream().map((s) -> {
+            System.out.println(s);
+            return s.toUpperCase();
+        }).collect(Collectors.toList());
+        collect.forEach(System.out::println);
+
+        System.out.println("================");
+
+        names.forEach(System.out::println);
+    }
+}
