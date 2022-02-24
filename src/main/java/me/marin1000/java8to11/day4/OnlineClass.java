9a9d5e0c9d039fb8211f70276ffeffaef0e5a746
1000marin <leexjin@gmail.com>
2022-02-24 10:37:48 +0900

    Java 8to11 : Optional API

---
+++ src/main/java/me/marin1000/java8to11/day4/OnlineClass.java
@@ -0,0 +1,49 @@
+package me.marin1000.java8to11.day4;
+
+import java.util.Optional;
+
+public class OnlineClass {
+    private Integer id;
+
+    private String title;
+
+    private boolean closed;
+
+    public Progress progress;
+
+    public OnlineClass(Integer id, String title, boolean closed) {
+        this.id = id;
+        this.title = title;
+        this.closed = closed;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isClosed() {
+        return closed;
+    }
+
+    public void setClosed(boolean closed) {
+        this.closed = closed;
+    }
+
+    public Optional<Progress>  getProgress() {
+//        return Optional.ofNullable(progress);   // ofNullable : 뒤에 들어오는것이 null 일수도 있을때 사용
+        return Optional.empty(); // null리턴 하지말고 이렇게 사용
+    }
+
+}
---
+++ src/main/java/me/marin1000/java8to11/day4/Progress.java
@@ -0,0 +1,18 @@
+package me.marin1000.java8to11.day4;
+
+import java.time.Duration;
+
+public class Progress {
+
+    private Duration studyDuration;
+
+    private boolean finished;
+
+    public Duration getStudyDuration() {
+        return studyDuration;
+    }
+
+    public void setStudyDuration(Duration studyDuration) {
+        this.studyDuration = studyDuration;
+    }
+}
