9fbbe1d2c5840642f2d68a62df08e5b84bbd02c5
1000marin <leexjin@gmail.com>
2022-02-26 09:09:50 +0900

    Java 8to11 : Date와 Time API

--- src/main/java/me/marin1000/java8to11/class5/App.java
+++ src/main/java/me/marin1000/java8to11/class5/App.java
@@ -7,10 +7,34 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+
+/**
+ * [ Date와 Time ] - Date와 Time 소개
+ *
+ * 자바 8에 새로운 날짜와 시간 API가 생긴 이유
+ *
+ * ● 그전까지 사용하던 java.util.Date 클래스는 mutable 하기 때문에 thead safe하지 않다.
+ * ● 클래스 이름이 명확하지 않다. Date인데 시간까지 다룬다.
+ * ● 버그 발생할 여지가 많다. (타입 안정성이 없고, 월이 0부터 시작한다거나..)
+ * ● 날짜 시간 처리가 복잡한 애플리케이션에서는 보통 ​Joda Time​을 쓰곤했다.
+ *
+ *  자바 8에서 제공하는 Date-Time API
+ *  ● JSR-310 스팩의 구현체를 제공한다.
+ *  ● 디자인 철학
+ *     ○ Clear
+ *     ○ Fluent
+ *     ○ Immutable
+ *     ○ Extensible
+ */
 public class App {
     public static void main(String[] args) throws InterruptedException {
         Date date = new Date();
-        Calendar calendar = new GregorianCalendar();
-        SimpleDateFormat dateFormat = new SimpleDateFormat();
+        long time = date.getTime();
+        System.out.println(time);   // 기계용 시간
+
+        Calendar jinBirthDay = new GregorianCalendar(1988, Calendar.JANUARY, 19);
+        System.out.println(jinBirthDay.getTime());
+        jinBirthDay.add(Calendar.DAY_OF_YEAR, 1); // 날짜 하루 뒤로
+        System.out.println(jinBirthDay.getTime());
     }
 }
---
+++ src/main/java/me/marin1000/java8to11/class5/App2.java
@@ -0,0 +1,22 @@
+package me.marin1000.java8to11.class5;
+
+import java.sql.SQLOutput;
+import java.time.*;
+
+public class App2 {
+    public static void main(String[] args) {
+        Instant instant = Instant.now();
+        System.out.println(instant);                            // 기준시 UTC, GMT
+        System.out.println(instant.atZone(ZoneId.of("UTC")));   // 기준시 UTC, GMT
+
+        ZoneId zone = ZoneId.systemDefault();
+        System.out.println(zone);
+        ZonedDateTime zonedDateTime = instant.atZone(zone);
+        System.out.println(zonedDateTime);
+
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
+        LocalDateTime birthDay =
+                LocalDateTime.of(1988, Month.JANUARY, 19, 0, 0, 0, 0);
+    }
+}
