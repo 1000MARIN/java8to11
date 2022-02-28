1f4c8cc217f1defaa746932eb243a885db460ab1
1000marin <leexjin@gmail.com>
2022-02-28 18:16:57 +0900

    Java 8to11 : CompletableFuture

--- src/main/java/me/marin1000/java8to11/class6/App.java
+++ src/main/java/me/marin1000/java8to11/class6/App.java
@@ -2,6 +2,15 @@ package me.marin1000.java8to11.class6;
 
 public class App {
     public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        myThread.start();
+        System.out.println("Hello" + Thread.currentThread().getName());
+    }
 
+    static class MyThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("Thread: " + Thread.currentThread().getName());
+        }
     }
 }
