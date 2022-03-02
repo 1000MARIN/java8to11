a08e112ab805b2ee68792079b14491c1d1a249fe
1000marin <leexjin@gmail.com>
2022-03-02 17:54:53 +0900

    Java 8to11 : CompletableFuture (Thread)

--- src/main/java/me/marin1000/java8to11/class6/App.java
+++ src/main/java/me/marin1000/java8to11/class6/App.java
@@ -1,6 +1,7 @@
 package me.marin1000.java8to11.class6;
 
 public class App {
+    // Thread 상속
     public static void main(String[] args) {
         MyThread myThread = new MyThread();
         myThread.start();
--- src/main/java/me/marin1000/java8to11/class6/App2.java
+++ src/main/java/me/marin1000/java8to11/class6/App2.java
@@ -1,13 +1,42 @@
 package me.marin1000.java8to11.class6;
 
 public class App2 {
-    public static void main(String[] args) {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
+    public static void main(String[] args) throws InterruptedException {
+        // Runnable 구현 또는 람다
+
+        //  현재 Thread sleep, 다른 Thread 먼저 실행
+        Thread thread1 = new Thread(() -> {
+            try {
+                Thread.sleep(1000L);    // 현재 Thread 재우기, 다른 Thread 먼저 실행
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Thread: " + Thread.currentThread().getName());
+        });
+        thread1.start();
+
+        // interupt : 다른 쓰레드 깨우기
+        Thread thread2 = new Thread(() -> {
+            while (true) {
                 System.out.println("Thread: " + Thread.currentThread().getName());
+                try {
+                    Thread.sleep(1000L);
+                } catch (InterruptedException e) {
+                    System.out.println("exit!");
+                    return;
+                }
             }
         });
+        thread2.start();
+
         System.out.println("Hello" + Thread.currentThread().getName());
+
+        // 3초 뒤에 thread2를 깨움
+        Thread.sleep(3000L);
+        thread2.interrupt();
+
+        // 다른 Thread를 기다림
+        thread2.join();
+        System.out.println(thread2 + " is finished");
     }
 }
