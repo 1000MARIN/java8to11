de63c10c84971410fe65d1a082231531a1c1edac
1000marin <leexjin@gmail.com>
2022-03-03 17:46:32 +0900

    Java 8to11 : 배열 Parallel 정렬 (Arrays.parallelSort())

---
+++ src/main/java/me/marin1000/java8to11/class8/App.java
@@ -0,0 +1,31 @@
+package me.marin1000.java8to11.class8;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
+/*
+    Arrays.parallelSort()
+        ● Fork/Join 프레임워크를 사용해서 배열을 병렬로 정렬하는 기능을 제공한다.
+
+    병렬 정렬 알고리즘
+        ● 배열을 둘로 계속 쪼갠다.
+        ● 합치면서 정렬한다.
+
+ */
+public class App {
+    public static void main(String[] args) {
+        int size = 1500;
+        int[] numbers = new int[size];
+        Random random = new Random();
+
+        IntStream.range(0, size).forEach(i -> numbers[i] = random.nextInt());
+        long start = System.nanoTime();
+        Arrays.sort(numbers);
+        System.out.println("serial sorting took " + (System.nanoTime() - start));
+
+        IntStream.range(0, size).forEach(i -> numbers[i] = random.nextInt());
+        start = System.nanoTime();
+        Arrays.parallelSort(numbers);
+        System.out.println("parallel sorting took " + (System.nanoTime() - start));
+    }
+}
